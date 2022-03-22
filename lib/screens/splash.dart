1502caadd8ed1fd2f48213fedb3a19ee03acbcfa
김영호 <ksemfn@gmail.com>
2022-03-22 12:41:34 +0900

    google fonts 적용, 스플래시 화면 구현

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,5 +1,6 @@
 import 'package:farming_manager/constants/colors.dart';
 import 'package:farming_manager/controller/splash/splash_view_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,23 +12,44 @@ class SplashScreen extends GetView<SplashViewModel> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          color: AppColors.green,
-          child:      Column(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: AppColors.green,
+      child: Column(
+        children: [
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(
-                  child: Container(
+              const Icon(
+                CupertinoIcons.leaf_arrow_circlepath,
+                color: AppColors.white,
+                size: 100,
+              ),
+              Text(
+                '농사 모아',
+                style: GoogleFonts.openSans(
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: AppColors.white),
+              ),
 
-
-                  )),
-              SizedBox(
-                height: 30,
-                child: Text(
-                  '이 앱은 공공기관에서 제공하는 데이터를 이용하고 있습니다.',
-                  style: GoogleFonts.lato(fontWeight: FontWeight.w400,fontSize: 13,color: AppColors.white),
-                ),
-              )
             ],
-          ),
-        ));
+          )),
+          SizedBox(
+            height: 30,
+            child:
+            Text(
+              '이 앱은 공공기관에서 제공하는 데이터를 이용하고 있습니다.',
+              style: GoogleFonts.lato(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 13,
+                  color: AppColors.white),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
