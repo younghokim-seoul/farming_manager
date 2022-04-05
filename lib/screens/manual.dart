a070612c501ca72b0f3efd2f4ece5efe9534abfe
김영호 <ksemfn@gmail.com>
2022-04-05 11:11:59 +0900

    관리 매뉴얼 , 주간 농사정보 scaffold 적용

--- lib/screens/manual.dart
+++ lib/screens/manual.dart
@@ -2,12 +2,20 @@ import 'package:farming_manager/controller/manual/manual_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../widgets/farming_text.dart';
+
 class ManualScreen extends GetView<ManualViewModel> {
 
   const ManualScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          title: const FarmingText(text: "품목별 관리매뉴얼", size: 16)),
+      body: Center(),
+    );
   }
 }
--- lib/screens/weekly_farm.dart
+++ lib/screens/weekly_farm.dart
@@ -2,12 +2,20 @@ import 'package:farming_manager/controller/manual/manual_view_model.dart';
 import 'package:farming_manager/controller/weeklyfarm/week_farm_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../widgets/farming_text.dart';
 class WeeklyFarmScreen extends GetView<WeeklyFarmViewModel> {
 
   const WeeklyFarmScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          title: const FarmingText(text: "주간 농사정보", size: 16)),
+      body: Center(),
+    );
   }
 }
