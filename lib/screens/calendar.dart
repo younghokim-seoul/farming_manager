08380a7b68b8f76cb19330fb5039c80042fb0856
김영호 <ksemfn@gmail.com>
2022-04-12 09:56:02 +0900

    메모 저장로직 작성. 날짜 필터링 로직적용 캘린더 stateful 변경

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -1,4 +1,9 @@
-import 'package:farming_manager/controller/calendar/calendar_view_model.dart';
+import 'dart:collection';
+
+import 'package:farming_manager/data/repository/farming_repository.dart';
+import 'package:farming_manager/data/request/memo_list_request.dart';
+import 'package:farming_manager/data/response/memo_list_response.dart';
+import 'package:farming_manager/di/app_module.dart';
 import 'package:farming_manager/router/routese.dart';
 import 'package:farming_manager/utils/utils.dart';
 import 'package:farming_manager/widgets/farming_text.dart';
@@ -9,12 +14,52 @@ import 'package:table_calendar/table_calendar.dart';
 
 import '../main.dart';
 
-class CalendarScreen extends GetView<CalendarViewModel> {
-  //
-  // // Map<DateTime, List<EventsModel>> _events;
-  // DateTime _selectedDay;
-
+class CalendarScreen extends StatefulWidget {
   const CalendarScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<CalendarScreen> {
+  final repository = locator.get<FarmingRepository>();
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  final events = LinkedHashMap<DateTime, List<MemoListResponse>>(equals: isSameDay);
+
+  Map<DateTime, List<MemoListResponse>> kEventSource = {};
+
+  List<MemoListResponse> _getEventsForDay(DateTime day) {
+    return events[day] ?? [];
+  }
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+    fetchMemoItems();
+  }
+
+  @override
+  void dispose() {
+
+    super.dispose();
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      var selectedTime = dataTimeToString(selectedDay);
+      Get.toNamed(Routes.MEMO, arguments: {"EXTRA_DATE": selectedTime});
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+      });
+
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,13 +68,13 @@ class CalendarScreen extends GetView<CalendarViewModel> {
           centerTitle: true,
           backgroundColor: Colors.transparent,
           title: const FarmingText(text: "달력", size: 16)),
-      body: SingleChildScrollView(child: Obx(() => _buildCalendarCustom())),
+      body: SingleChildScrollView(child: _buildCalendarCustom()),
     );
   }
 
   Widget _buildCalendarCustom() {
     return TableCalendar(
-      focusedDay: controller.focusedDay,
+      focusedDay: _focusedDay,
       firstDay: kFirstDay,
       lastDay: kLastDay,
       locale: 'ko-KR',
@@ -38,23 +83,14 @@ class CalendarScreen extends GetView<CalendarViewModel> {
       calendarFormat: CalendarFormat.month,
       calendarBuilders: _calendarBuilder(),
       headerVisible: true,
-      headerStyle:
-          const HeaderStyle(formatButtonVisible: false, titleCentered: true),
-      selectedDayPredicate: (day) {
-        return isSameDay(controller.selectedDay, day);
-      },
-      onDaySelected: (selectedDay, focusedDay) {
-        if (!isSameDay(controller.selectedDay, selectedDay)) {
-          controller.setSelectedDay(selectedDay);
-          controller.setFocusedDay(focusedDay);
-
-          logger.i(":::now selectedDay -> " + selectedDay.toString());
-          var selectedTime = dataTimeToString(selectedDay);
-          Get.toNamed(Routes.MEMO, arguments: {"EXTRA_DATE": selectedTime});
-
-          logger.i(":::now focusedDay -> " + selectedTime);
-        }
-      },
+      headerStyle: const HeaderStyle(formatButtonVisible: false, titleCentered: true),
+      selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+      onDaySelected: _onDaySelected,
+      eventLoader: _getEventsForDay,
+        onPageChanged: (focusedDay) {
+          _focusedDay = focusedDay;
+          fetchMemoItems();
+        },
     );
   }
 
@@ -71,4 +107,28 @@ class CalendarScreen extends GetView<CalendarViewModel> {
           ));
     });
   }
+
+  void fetchMemoItems() async {
+    int year = _focusedDay.year;
+    int month = _focusedDay.month;
+    final response = await repository.getMemoList(MemoListRequest(wYear: year, wMonth: month));
+
+    response.when(success: (response) {
+      // logger.i(response);
+
+      if(response.isNotEmpty){
+        for (var element in response) {
+          kEventSource[DateTime(element.wYear,element.wMonth,element.wDay)] = [element];
+        }
+
+        setState(() {
+          events.addAll(kEventSource);
+        });
+      }
+      // logger.e("[fetchMemoItems] events -> " + events.length.toString());
+    }, error: (error) {
+      logger.e("[fetchMemoItems] Api Error -> $error");
+    });
+  }
+
 }
